46a0c0565955cb6f5b33c7972ab8ae5162875e1d
Keerthivasan-04 <a.keerthi2004@gmail.com>
2026-03-26 12:41:17 +0530

    fix: update security config file

--- nestmanager-backend/src/main/java/com/nestmanager/security/SecurityConfig.java
+++ nestmanager-backend/src/main/java/com/nestmanager/security/SecurityConfig.java
@@ -65,8 +65,8 @@ public class SecurityConfig {
                 "http://127.0.0.1:5501",
                 "http://localhost:5501",
                 "http://127.0.0.1:3000",
-                "http://localhost:3000"
-                "https://nestmanager.netlify.app"
+                "http://localhost:3000",
+                "https://nestmanager.netlify.app",
                 "https://nestmanager.netlify.app/pages/login/login.html"
         ));
 
